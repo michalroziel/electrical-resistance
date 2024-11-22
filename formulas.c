c4c75398e811e480f4e28f3df7f01c9c94773e78
Michał Roziel <rozielmichal@gmail.com>
2024-11-22 16:23:47 +0100

    added functionality of input

--- formulas.c
+++ formulas.c
@@ -5,6 +5,7 @@
 	void start(void){
 
 	int loopW = 0 ;
+	int loopS = 0;
 
 	int n;
 	char schaltung;
@@ -16,12 +17,10 @@
 	scanf("%d", &inputW );
 
 	if (inputW<= 0){		
-	printf("Die anzahl der Widerstände kann nicht kleiner oder gleich 0 sein! \n Bitte geben Sie etwas anderes an!");	
-			
+	printf("Die anzahl der Widerstände kann nicht kleiner oder gleich 0 sein! \n Bitte geben Sie etwas anderes an!");
 		}
 
 	else{
-		
 	loopW = 1;
 	n = inputW;
 		}
@@ -29,6 +28,29 @@
 
 	printf("Die Anzahl Der Widerstände beträgt : %d \n" , n); 
 
+	printf("Bitte geben Sie die Art der Schaltung an : \n  \n P = Parallel\n S = Seriell\n" );
 
+
+	while(loopS==0) {
+	char inputS;
+
+		scanf(" %c", &inputS );
+
+		switch (inputS) {
+			case 'S':
+				schaltung = 'S';
+				loopS = 1;
+				break;
+			case 'P':
+				schaltung = 'P';
+				loopS = 1;
+				break;
+			default:
+				printf("Ungültige Eingabe. ");
+		}
+
+	}
+
+		printf("Die Art der Ausgewählten Schaltung ist : %c", schaltung);
 	
 }
